253e4871e3ccfcda92dad54d34aa9e20358eaa19
Jiksi Michael <jiksimichael@macbook-pro.mynetworksettings.com>
2026-05-27 23:59:33 -0400

    Fix desktop layout with media queries

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
+import './desktop.css'
 import Link from 'next/link'
 
 const montserrat = Montserrat({ subsets: ['latin'], weight: ['400','500','600','700','800','900'] })
